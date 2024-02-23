46d81875258befc8c46d449cbd82120e89dc7260
Nathan <nattybe6@gmail.com>
2024-02-23 18:25:28 +0000

    some stuff

--- source/main.cpp
+++ source/main.cpp
@@ -9,9 +9,15 @@ int main(){
 	videoSetMode(MODE_5_2D);//set video mode main
 	videoSetModeSub(MODE_5_2D);
   lcdMainOnTop(); //set main screen to top
+
   SpriteController* spriteController = new SpriteController();
+  int sprite1 = spriteController->createNewSprite(true, SpriteSize_16x16, 50, 50, 1);
+  spriteController->addPalette(0, true, blue_head_downPal, blue_head_downPalLen);
+  spriteController->setSpritePaletteNumber(0, true, 0);
+  spriteController->setSpriteTiles(0, true, blue_head_downTiles, blue_head_downTilesLen);
 
   while (1){
+    spriteController->updateDisplays(true, false);
 
   }
 
